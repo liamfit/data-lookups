bb3ef9e959b312b2536b42f2eef46e76465d77f6
liamfit <lum.fitzpatrick@gmail.com>
2024-01-04 11:38:41 +0000

    add latest updates to pipelines and tf config

--- data.tf
+++ data.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+
 data "aws_vpc" "this" {
   count = contains(var.resources, "vpc") ? 1 : 0
   tags  = var.tags
@@ -13,7 +15,13 @@ data "aws_subnets" "this" {
   }
 }
 
-data "aws_resourcegroupstaggingapi_resources" "ecs" {
+data "aws_vpc_endpoint" "apigw" {
+  count        = contains(var.resources, "vpce-apigw") ? 1 : 0
+  service_name = "com.amazonaws.${data.aws_region.current.name}.execute-api"
+  vpc_id       = data.aws_vpc.this[0].id
+}
+
+data "aws_resourcegroupstaggingapi_resources" "ecs_cluster" {
   count                 = contains(var.resources, "ecs") ? 1 : 0
   resource_type_filters = ["ecs:cluster"]
 
@@ -26,8 +34,8 @@ data "aws_resourcegroupstaggingapi_resources" "ecs" {
   }
 }
 
-data "aws_resourcegroupstaggingapi_resources" "alb" {
-  count                 = contains(var.resources, "alb") ? 1 : 0
+data "aws_resourcegroupstaggingapi_resources" "lb" {
+  count                 = contains(var.resources, "lb") ? 1 : 0
   resource_type_filters = ["elasticloadbalancing:loadbalancer"]
 
   dynamic "tag_filter" {
@@ -39,13 +47,47 @@ data "aws_resourcegroupstaggingapi_resources" "alb" {
   }
 }
 
-data "aws_lb_listener" "this" {
-  count             = contains(var.resources, "alb") ? 1 : 0
-  load_balancer_arn = data.aws_resourcegroupstaggingapi_resources.alb[0].resource_tag_mapping_list[0].resource_arn
+data "aws_lb_listener" "port_80" {
+  count             = contains(var.resources, "lb") ? 1 : 0
+  load_balancer_arn = data.aws_resourcegroupstaggingapi_resources.lb[0].resource_tag_mapping_list[0].resource_arn
   port              = 80
+}
+
+data "aws_lb_listener" "port_443" {
+  count             = contains(var.resources, "lb") ? 1 : 0
+  load_balancer_arn = data.aws_resourcegroupstaggingapi_resources.lb[0].resource_tag_mapping_list[0].resource_arn
+  port              = 443
 }
 
 data "aws_security_group" "this" {
   count = contains(var.resources, "sg") ? 1 : 0
   tags  = var.tags
+}
+
+data "aws_resourcegroupstaggingapi_resources" "acm_certificate" {
+  count    = contains(var.resources, "acm") ? 1 : 0
+
+  resource_type_filters = ["acm:certificate"]
+
+  dynamic "tag_filter" {
+    for_each = var.tags
+    content {
+      key    = tag_filter.key
+      values = [tag_filter.value]
+    }
+  }
+}
+
+data "aws_resourcegroupstaggingapi_resources" "dms_replication_subnet_group" {
+  count    = contains(var.resources, "dms") ? 1 : 0
+
+  resource_type_filters = ["dms:subgrp"]
+
+  dynamic "tag_filter" {
+    for_each = var.tags
+    content {
+      key    = tag_filter.key
+      values = [tag_filter.value]
+    }
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -6,22 +6,38 @@ output "vpc_subnets" {
   value = length(data.aws_subnets.this) > 0 ? data.aws_subnets.this[0].ids : null
 }
 
+output "vpc_endpoint_id_apigw" {
+  value = length(data.aws_vpc_endpoint.apigw) > 0 ? data.aws_vpc_endpoint.apigw[0].id : null
+}
+
 output "ecs_cluster_arn" {
-  value = length(data.aws_resourcegroupstaggingapi_resources.ecs) > 0 ? data.aws_resourcegroupstaggingapi_resources.ecs[0].resource_tag_mapping_list[0].resource_arn : null
+  value = length(data.aws_resourcegroupstaggingapi_resources.ecs_cluster) > 0 ? data.aws_resourcegroupstaggingapi_resources.ecs_cluster[0].resource_tag_mapping_list[0].resource_arn : null
 }
 
 output "ecs_cluster_name" {
-  value = length(data.aws_resourcegroupstaggingapi_resources.ecs) > 0 ? reverse(split("/", data.aws_resourcegroupstaggingapi_resources.ecs[0].resource_tag_mapping_list[0].resource_arn ))[0] : null
+  value = length(data.aws_resourcegroupstaggingapi_resources.ecs_cluster) > 0 ? reverse(split("/", data.aws_resourcegroupstaggingapi_resources.ecs_cluster[0].resource_tag_mapping_list[0].resource_arn))[0] : null
 }
 
-output "alb_arn" {
-  value = length(data.aws_resourcegroupstaggingapi_resources.alb) > 0 ? data.aws_resourcegroupstaggingapi_resources.alb[0].resource_tag_mapping_list[0].resource_arn : null
+output "lb_arn" {
+  value = length(data.aws_resourcegroupstaggingapi_resources.lb) > 0 ? data.aws_resourcegroupstaggingapi_resources.lb[0].resource_tag_mapping_list[0].resource_arn : null
 }
 
-output "alb_listener_arn" {
-  value = length(data.aws_lb_listener.this) > 0 ? data.aws_lb_listener.this[0].arn : null
+output "lb_listener_80_arn" {
+  value = length(data.aws_lb_listener.port_80) > 0 ? data.aws_lb_listener.port_80[0].arn : null
+}
+
+output "lb_listener_443_arn" {
+  value = length(data.aws_lb_listener.port_443) > 0 ? data.aws_lb_listener.port_443[0].arn : null
 }
 
 output "security_group" {
   value = length(data.aws_security_group.this) > 0 ? data.aws_security_group.this[0].id : null
+}
+
+output "acm_certificate_arn" {
+  value = length(data.aws_resourcegroupstaggingapi_resources.acm_certificate) > 0 ? data.aws_resourcegroupstaggingapi_resources.acm_certificate[0].resource_tag_mapping_list[0].resource_arn : null
+}
+
+output "dms_replication_subnet_group_arn" {
+  value = length(data.aws_resourcegroupstaggingapi_resources.dms_replication_subnet_group) > 0 ? data.aws_resourcegroupstaggingapi_resources.dms_replication_subnet_group[0].resource_tag_mapping_list[0].resource_arn : null
 }
